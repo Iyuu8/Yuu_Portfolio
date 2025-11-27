c537740bea8cb11a9a03dab36571cab5f9e4e999
Iyuu8 <anesbenaziza19@gmail.com>
2025-11-27 12:41:55 +0100

    version 3 home completed

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -7,15 +7,18 @@ export default function Home() {
     <section className='min-h-[calc(100vh-5rem)] flex flex-col justify-start gap-[0.5vh] items-center'>
 
       {/*mobile design*/}
-      <h1 className='text-[clamp(32px,4.4vh,35px)] md:text-[4.3vh] font-bold text-[var(--dark-red)] text-center w-[95%] lg:hidden'>Hello, I am Ayoub!</h1>
-      <div className='w-[clamp(68%,315px,90%)] max-w-[440px] aspect-square  lg:hidden'>
-        <img src="/pfp_home.png" alt="profile picture"/>
+      <div className='w-full flex flex-col justify-start gap-[0.5vh] items-center lg:hidden ipad'>
+        <h1 className='text-[clamp(32px,4.4vh,35px)] md:text-[4.3vh] font-bold text-[var(--dark-red)] text-center w-[95%] '>Hello, I am Ayoub!</h1>
+        <div className='w-[clamp(68%,315px,90%)] max-w-[440px] aspect-square  '>
+          <img src="/pfp_home.png" alt="profile picture"/>
+        </div>
+        <h1 className='text-[4.9vh] font-bold text-[var(--dark-red)] text-center w-[90%] '>
+          A Webdev&
+          UI/UX Designer
+        </h1>
+
       </div>
-      <h1 className='text-[4.9vh] font-bold text-[var(--dark-red)] text-center w-[90%] lg:hidden'>
-        A Webdev&
-        UI/UX Designer
-      </h1>
-      <div className='grow-1 flex items-center w-full justify-center lg:hidden'>
+      <div className='grow-1 flex items-center w-full justify-center lg:hidden ipad'>
         <button className='bg-[var(--strong-red)] text-[var(--background)] text-[3.8vh] font-bold px-[0.6rem] py-[0.4rem] rounded-[0.5rem]'>
           <a href="#About">Explore</a>
         </button>
@@ -27,7 +30,7 @@ export default function Home() {
       </h2>
 
       {/*laptop version */}
-      <div className='hidden lg:flex w-[90%] justify-between min-h-[70vh]'>
+      <div className='hidden laptop w-[90%] justify-between min-h-[70vh]'>
         <div className='flex-col flex'>
           <div className='flex flex-col text-[var(--dark-red)] grow justify-center pt-[10vh]'>
             <h1 className='font-bold text-[clamp(48px,4vw,55px)]'>Hello, I am Ayoub</h1>
@@ -41,7 +44,7 @@ export default function Home() {
           </div>
           <div className='h-[8rem] flex items-center justify-center relative'>
             <div className='border-1 absolute top-[50%] left-[50%] translate-[-50%] bg-[var(--weak-red)] font-semibold text-[4vh] py-[0.3rem] px-[1.5rem] text-[var(--weak-red)] rounded-[0.5rem]'>Explore</div>
-            <button className='cursor-pointer translate-[-0.5rem] border-2 text-[var(--dark-red)] font-semibold text-[4vh] py-[0.3rem] px-[1.5rem] rounded-[0.5rem] transition-all duration-300 hover:translate-0'><a href="#About">Explore</a></button>
+            <button className='cursor-pointer translate-[-0.5rem] border-2 text-[var(--dark-red)] font-semibold text-[4vh] py-[0.3rem] px-[1.5rem] rounded-[0.5rem] transition-all duration-300 hover:translate-0 active:translate-0 '><a href="#About">Explore</a></button>
             
           </div>
         </div>
@@ -58,6 +61,7 @@ export default function Home() {
       <div className='hidden lg:flex grow-1 items-center text-center w-[95%] justify-center text-[3.5vh] font-semibold'>
         Continuously learning and exploring new web technologies.
       </div>
+
 
 
         
